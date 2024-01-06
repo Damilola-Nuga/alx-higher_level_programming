a5dc7efeab32869ffa01c59af10b6f4039470fb2
Damilola-Nuga <damiloladaniel357@gmail.com>
2024-01-06 14:03:50 -0800

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     length = len(sentence)
     if len == 0:
-        first_let = None
+        first_let = "None"
     else:
         first_let = sentence[0]
     new_tuple = (length, first_let)
