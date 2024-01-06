34e1b2a089b4c0f13e427d20666c181cadde3a3f
Damilola-Nuga <damiloladaniel357@gmail.com>
2024-01-06 14:15:56 -0800

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,8 @@
 
 def multiple_returns(sentence):
     length = len(sentence)
-    if len == 0:
-        first_let = "None"
+    if length == 0:
+        first_let = None
     else:
         first_let = sentence[0]
     return length, first_let
